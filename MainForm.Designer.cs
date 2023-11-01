e07f3b184a4ec3eda9def0010b34e6dfd535838b
Michael Johne <mjohne@users.noreply.github.com>
2023-11-01 20:47:19 +0100

    Renamed "Form1" to "MainForm"

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Universal_List_Generator
 {
-	partial class Form1
+	partial class MainForm
 	{
 		/// <summary>
 		///  Required designer variable.
@@ -28,10 +28,16 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			SuspendLayout();
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(715, 447);
+			Name = "Form1";
+			Text = "Form1";
+			ResumeLayout(false);
 		}
 
 		#endregion
